e3253fc9506085c964f8a01847a945e10eb63cc6
Gaurav Jadhav <gaurav.jadhav@kuliza.com>
2022-09-16 23:52:36 +0530

    Other parts of a project

---
+++ btc-tracker/README.md
@@ -0,0 +1,7 @@
+# Read Me
+The following documents mentions required settings to run this project
+
+# Getting Started
+
+### Reference Documentation
+Please set below environment variable to run this project
---
+++ btc-tracker/src/test/java/com/crypto/btctracker/BtcTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.btctracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
