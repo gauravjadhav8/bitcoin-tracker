3a22084ea2899e65675af7f9bc1e4fc52dc1c1e5
Gaurav Jadhav <gaurav.jadhav@kuliza.com>
2022-09-16 23:46:43 +0530

    Added Entities to persist data in DB

---
+++ btc-tracker/src/main/java/com/crypto/btctracker/entities/PriceTracker.java
@@ -0,0 +1,52 @@
+package com.crypto.btctracker.entities;
+
+import com.crypto.btctracker.listeners.PriceTrackEventListener;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name="price_tracker")
+@ToString
+@NoArgsConstructor
+@Getter
+@EntityListeners(PriceTrackEventListener.class)
+public class PriceTracker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+            @JsonIgnore
+    long id;
+
+    @CreationTimestamp
+    @JsonSerialize
+    Date timestamp;
+
+    @CreationTimestamp
+    @JsonIgnore
+    LocalDate date;
+
+    @Column(name = "coin_name")
+    @NotNull
+            @JsonSerialize
+    String coinName;
+
+    @Column(name = "coin_price")
+    @NotNull
+    @JsonSerialize
+    Double coinPrice;
+
+    public PriceTracker(String name, Double price){
+        this.coinName = name;
+        this.coinPrice = price;
+    }
+
+}
